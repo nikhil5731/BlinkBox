eb9c3633a77d377a12f66e6de7643ae74237cd87
nikhil5731 <nikhilgraphicd@gmail.com>
2023-11-11 21:10:37 +0530

    bug 2

--- components/setEmail.tsx
+++ components/setEmail.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-export default function setEmail() {
+export default function SetEmail() {
   const [email, setEmail] = useState<string | null>("Generate New Emails");
   const [loader, setLoader] = useState(false);
   const [login, setLogin] = useState<string | undefined>("");
